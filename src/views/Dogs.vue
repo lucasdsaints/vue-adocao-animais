560561f6fddcb596a179bf5732073354b0ea01a7
lucasdsaints <38575761+lucasdsaints@users.noreply.github.com>
2020-02-18 16:44:40 -0300

    estruturacao

---
+++ src/views/Dogs.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Dogs for Adoption</h1>
+    <b-table striped hover :items="dogs"></b-table>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+      // dogs
+    }
+  },
+  computed: {
+    ...mapState([
+      'dogs'
+    ])
+  }
+}
+</script>
